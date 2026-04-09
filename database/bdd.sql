fce370ffd90c928266ab10657e30540049a8a16d
Lucas Bertoloni <lucasbertoloni5@gmail.com>
2026-04-08 22:06:44 -0300

    add: muestra de jerarquia de dirs

---
+++ database/bdd.sql
@@ -0,0 +1,1 @@
+-- Lucas --> yo podria hacer esto
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,3 +8,8 @@ def index():
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
+
+# Hay que hacer 15 endpoints totales
+# Ver tema de paginación y filtro
+# Agregar el Swagger
+# README indicando el funcionamiento de cada endpoint + ejecución, etc 
