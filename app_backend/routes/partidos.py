78e7d04ee888011aac07f2affe3e1ed623ba3518
santiagotrios <strios@fi.uba.ar>
2026-04-09 20:38:33 -0300

    intente hacer un endpoint de partidos

--- app_backend/routes/partidos.py
+++ app_backend/routes/partidos.py
@@ -1,3 +1,92 @@
 from flask import Blueprint, jsonify, request
+from db import get_connection
 
 partidos_bp = Blueprint("partidos", __name__)
+
+@partidos_bp.route ("/partidos", methods=["GET"])
+def lista_partidos():
+    conn = get_connection()
+    cursor = conn.cursor (dictionary=True)
+
+    try:
+        fecha = request.args.get ("fecha")
+        fase = request.args.get ("fase")
+        equipo = request.args.get ("equipo")
+        limit = int (request.args.get("_limit", 10))
+        offset = int (request.args.get("_offset", 0))
+
+        consulta_partidos= """
+        SELECT id, equipo_local, equipo_visitante, fecha, fase
+        FROM partidos
+        WHERE 1=1
+        """
+
+        valores = []
+
+        if equipo:
+            consulta_partidos += " AND (equipo_local = %s OR equipo_visitante = %s)"
+            valores.extend ([equipo, equipo])
+
+        if fecha:
+            consulta_partidos += " AND fecha = %s"
+            valores.append (fecha)
+
+        if fase:
+            consulta_partidos += " AND fase = %s"
+            valores.append (fase)
+
+        consulta_cantidad_partidos = f"SELECT (*) as total from ({consulta_partidos}) as sub" 
+        cursor.execute(consulta_cantidad_partidos, valores) 
+        total = cursor.fetchone()["total"]
+
+        consulta_partidos += " LIMIT %s, OFFSET %s"
+        valores.extend ([limit, offset])  
+
+        cursor.execute (consulta_partidos, valores)
+        lista_partidos = cursor.fetchall()
+
+        partidos = []
+        for partido in lista_partidos:
+            partidos.append ({
+                "id" : partido["id"],
+                "equipo_local" : partido ["equipo_local"],
+                "equipo_visitante" : partido ["equipo_visitante"],
+                "fecha" : str(partido["fecha"]),
+                "fase" : partido ["fase"]
+            })
+
+        base_url = request.base_url
+
+        def build_url (new_offset):
+            return f"{base_url}?_limit={limit}&_offset={new_offset}"
+
+        links = {
+            "_first" : {"href": build_url(0)},
+            "_last" :  {"href": build_url(max(total - limit, 0))}
+        }
+
+        if offset > 0:
+            links ["_prev"] = {"href": build_url(max(total - limit, 0))}
+
+
+        if offset + limit < total: 
+            links ["_next"] = {"href": build_url(offset + limit)}
+
+        return jsonify ({
+            "partidos": partidos,
+            "_links" : links,
+        }), 200
+
+    except Exception as e:
+        return jsonify ({"error": str(e)}), 500
+    
+    finally:
+        cursor.close()
+        conn.close()
+            
+
+
+
+        
+
+    
